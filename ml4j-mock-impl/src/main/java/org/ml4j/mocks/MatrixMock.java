db584ec77344628e1c2bd5cd3a007ffbc70e2346
Michael Lavelle <michael@lavelle.name>
2017-09-30 13:25:17 +0100

    Adding minimal mocks to support AutoEncoder test harness

---
+++ ml4j-mock-impl/src/main/java/org/ml4j/mocks/MatrixMock.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ml4j.mocks;
+
+import org.ml4j.Matrix;
+
+/**
+ * A skeleton mock Matrix for structural purposes.
+ * 
+ * @author Michael Lavelle
+ */
+public class MatrixMock implements Matrix {
+
+  /**
+   * Default serialization id.
+   */
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Number of rows in the Matrix.
+   */
+  private int rows;
+  
+  /**
+   * Number of columns in the Matrix.
+   */
+  private int columns;
+
+  /**
+   * Construct a new MatrixMock in the specified shape.
+   * 
+   * @param rows number of rows in the Matrix
+   * @param columns number of columns in the Matrix
+   */
+  public MatrixMock(int rows, int columns) {
+    this.rows = rows;
+    this.columns = columns;
+  }
+
+  @Override
+  public int getRows() {
+    return rows;
+  }
+
+  @Override
+  public int getColumns() {
+    return columns;
+  }
+}
---
+++ ml4j-mock-impl/src/main/java/org/ml4j/nn/unsupervised/mocks/AutoEncoderMock.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ml4j.nn.unsupervised.mocks;
+
+import org.ml4j.nn.layers.FeedForwardLayer;
+import org.ml4j.nn.neurons.NeuronsActivation;
+import org.ml4j.nn.unsupervised.AutoEncoder;
+import org.ml4j.nn.unsupervised.AutoEncoderContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple mock implementation of AutoEncoder consisting of 2 mock FeedForwardLayers.
+ *
+ * @author Michael Lavelle
+ */
+public class AutoEncoderMock implements AutoEncoder {
+
+  /**
+   * Default serialization id.
+   */
+  private static final long serialVersionUID = 1L;
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(AutoEncoderMock.class);
+  
+  private FeedForwardLayer<?> encodingLayer;
+  private FeedForwardLayer<?> decodingLayer;
+  
+  public AutoEncoderMock(FeedForwardLayer<?> encodingLayer, FeedForwardLayer<?> decodingLayer) {
+    this.encodingLayer = encodingLayer;
+    this.decodingLayer = decodingLayer;
+  }
+ 
+  @Override
+  public void train(NeuronsActivation trainingDataActivations, AutoEncoderContext trainingContext) {
+    LOGGER.debug("Mock training AutoEncoderMock - no op for now");
+  }
+
+  @Override
+  public List<FeedForwardLayer<?>> getLayers() {
+    List<FeedForwardLayer<?>> layers = new ArrayList<>();
+    layers.add(encodingLayer);
+    layers.add(decodingLayer);
+    return layers;
+  }
+
+  @Override
+  public int getNumberOfLayers() {
+    return 2;
+  }
+
+  @Override
+  public FeedForwardLayer<?> getLayer(int layerIndex) {
+    return getLayers().get(layerIndex);
+  }
+
+  @Override
+  public FeedForwardLayer<?> getFirstLayer() {
+    return encodingLayer;
+  }
+
+  @Override
+  public FeedForwardLayer<?> getFinalLayer() {
+    return decodingLayer;
+  }
+
+  @Override
+  public AutoEncoder dup() {
+    return new AutoEncoderMock(encodingLayer.dup(), decodingLayer.dup());
+  }
+}
