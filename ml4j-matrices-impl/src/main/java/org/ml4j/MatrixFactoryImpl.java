ae72f14752338c349a4ddb925baba2e7a7c4c658
Michael Lavelle <michael@lavelle.name>
2017-11-09 10:07:38 +0000

    Completing removal of matrix mocks

--- ml4j-matrices-impl/src/main/java/org/ml4j/MatrixFactoryImpl.java
+++ ml4j-matrices-impl/src/main/java/org/ml4j/MatrixFactoryImpl.java
@@ -1,39 +1,40 @@
 /*
  * Copyright 2017 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.ml4j;
 
 import org.ml4j.Matrix;
 import org.ml4j.MatrixFactory;
-import org.ml4j.mocks.MatrixFactoryMock;
 
 /**
- * The default ml4j MatrixFactory.
+ * A skeleton MatrixFactory impl.
  * 
  * @author Michael Lavelle
  */
 public class MatrixFactoryImpl implements MatrixFactory {
 
-  private MatrixFactory delegatedFactory;
-
-  public MatrixFactoryImpl() {
-    this.delegatedFactory = new MatrixFactoryMock();
-  }
-
   @Override
   public Matrix createOnes(int rows, int columns) {
-    return delegatedFactory.createOnes(rows, columns);
+    double[][] data = new double[rows][columns];
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < columns; c++) {
+        data[r][c] = 1;
+      }
+    }
+    return createMatrix(data);
   }
   
   @Override
@@ -41,23 +42,24 @@ public class MatrixFactoryImpl implements MatrixFactory {
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
+
   @Override
   public Matrix createMatrix(double[][] data) {
-    return delegatedFactory.createMatrix(data);
+    return new MatrixImpl(data);
   }
   
   @Override
-  public Matrix createMatrix(double[] data) {
+  public Matrix createMatrix() {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public Matrix createMatrix(double[] arg0) {
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
   @Override
   public Matrix createMatrix(int arg0, int arg1, double[] arg2) {
-    throw new UnsupportedOperationException("Not implemented yet");
-  }
-
-  @Override
-  public Matrix createMatrix() {
     throw new UnsupportedOperationException("Not implemented yet");
   }
 
@@ -68,7 +70,7 @@ public class MatrixFactoryImpl implements MatrixFactory {
 
   @Override
   public Matrix createZeros(int rows, int columns) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    return new MatrixImpl(new double[rows][columns]);
   }
 
   @Override
@@ -80,7 +82,7 @@ public class MatrixFactoryImpl implements MatrixFactory {
   public Matrix createHorizontalConcatenation(Matrix arg0, Matrix arg1) {
     throw new UnsupportedOperationException("Not implemented yet");
   }
-  
+ 
   @Override
   public Matrix createRand(int arg0, int arg1) {
     throw new UnsupportedOperationException("Not implemented yet");
